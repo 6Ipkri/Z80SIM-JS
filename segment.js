f82d3e85507e10b3cb911c640441468c62ef0942
Akrd Kmnc <akrd.kmnc@kkumail.com>
2019-05-14 12:18:07 +0700

    add dot

--- segment.js
+++ segment.js
@@ -8,10 +8,26 @@ segment = n => {
 
 volume = flag => {
     var v = document.getElementById('volume');
-    v.className = "fas fa-volume-up volume-" + ((flag) ? "on" : "off");
+    v.className = "fas fa-volume-up icon-" + ((flag) ? "on" : "off");
+}
+
+dot = (n, flag) => {
+    var d = document.getElementById('dot' + n);
+    d.className = "fas fa-circle icon-" + ((flag) ? "on" : "off");
+}
+
+dot_only = (n) => {
+    var j;
+    for (j = 1; j < 10; j++) {
+        dot(j, (n == j));
+    }
 }
 
 $(document).ready(function () {
     var i = 0;
-    setInterval(function () { segment(i++); volume((i % 2 == 0) ? true : false) }, 1000);
+    setInterval(function () {
+        segment(i++);
+        volume((i % 2 == 0) ? true : false);
+        dot_only(i % 9 + 1)
+    }, 1000);
 })
